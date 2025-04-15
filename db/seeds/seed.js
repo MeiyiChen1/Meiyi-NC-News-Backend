dcc9d5da7e296c41cc6c2befd8acf25f573e8348
Meiyi <meiyi@Meiyis-MacBook-Pro.local>
2025-04-15 23:30:47 +0100

    update first two tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,30 @@
-const db = require("../connection")
+const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db.query(`DROP TABLE IF EXISTS comments`).then(() => {
+    return db.query(`DROP TABLE IF EXISTS articles`).then(() => {
+      return db
+        .query(`DROP TABLE IF EXISTS users`)
+        .then(() => {
+          return db.query(`DROP TABLE IF EXISTS topics`);
+        })
+        .then(() => {
+          return db.query(`CREATE TABLE topics(
+            slug VARCHAR(100) PRIMARY KEY,
+            description VARCHAR(100),
+            img_url VARCHAR(1000))`);
+        })
+        .then(() => {
+          return db.query(`CREATE TABLE users(
+            username VARCHAR(80) PRIMARY KEY,
+            name VARCHAR(80),
+            avatar_url VARCHAR(1000))`);
+        });
+    });
+  }); //<< write your first query in here.
 };
+
+const format = require("pg-format");
 module.exports = seed;
+
+//topics, users, articles, and comments tables
