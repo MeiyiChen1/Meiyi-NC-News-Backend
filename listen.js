05e206a098daf651fd840abc64962934a5e7ca7a
Meiyi <meiyi@Meiyis-MacBook-Pro.local>
2025-04-28 12:24:24 +0100

    getting the api and pass the test

---
+++ listen.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+
+app.listen(9090, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is listening on port 9090...");
+  }
+});
+
+module.exports = app;
