a4f2a2c375c767445258d11817be6b61c85602cd
Meiyi <meiyi@Meiyis-MacBook-Pro.local>
2025-05-01 16:57:07 +0100

    change

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,17 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
+} else {
+  console.log(
+    `Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`
+  );
+}
+
 const config = {};
 
 if (ENV === "production") {
@@ -9,14 +20,6 @@ if (ENV === "production") {
 }
 module.exports = new Pool(config);
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+//const db = new Pool();
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("No PGDATABASE or DATABASE_URL configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
-
-module.exports = db;
+//module.exports = db;
