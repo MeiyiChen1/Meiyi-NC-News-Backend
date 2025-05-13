583afc8c6b72dd79080563b4293bfa8a32f6f091
Meiyi Chen <116582515+MeiyiChen1@users.noreply.github.com>
2025-05-13 22:44:19 +0100

    Update comments.models.js

--- models folder/comments.models.js
+++ models folder/comments.models.js
@@ -46,12 +46,6 @@ const deleteComment = (comment_id) => {
   return db
     .query("SELECT * FROM comments WHERE comment_id = $1", [comment_id])
     .then(({ rows }) => {
-      if (!rows) {
-        return Promise.reject({
-          status: 404,
-          msg: "Comment not found",
-        });
-      }
       if (rows.length === 0) {
         return null;
       }
